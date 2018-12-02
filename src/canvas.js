b743f04cef0ad0c07bc1d8197faf9e506b1ac49d
Marcin Brumer <brumek31@gmail.com>
2018-12-02 15:28:42 +0100

    Added temporary ground

--- src/canvas.js
+++ src/canvas.js
@@ -28,12 +28,19 @@ const backgroundGradient = c.createLinearGradient(0, 0, 0, canvas.height);
 backgroundGradient.addColorStop(0, "#171e26");
 backgroundGradient.addColorStop(1, "#3f586b");
 
+let groundHeight = 100;
+
 function init() {}
 
 // Animation Loop
 function animate() {
+  // Background
   c.fillStyle = backgroundGradient;
   c.fillRect(0, 0, canvas.width, canvas.height);
+  // Ground
+  c.fillStyle = "#0D0909";
+  c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight);
+
   requestAnimationFrame(animate);
 }
 
